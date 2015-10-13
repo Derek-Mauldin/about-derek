8569085aec69b798e58fe5e0499e8116fb2ee5c4
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-10-13 12:51:44 -0600

    Added images and changes to index.php

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
-	<html>
+	<html lang="en">
 		<head>
+			<meta charset="utf-8" />
 			<title>About Derek</title>
 		</head>
 		<body>
-			<p>About Derek</p>
+			<header>
+				<h1><img src="img/weird.jpg" alt="Weird" height="100" width="100"/>    Derek Mauldin</h1>
+					<p>
+						<em><a href="https://bootcamp-coders.cnm.edu/" target="_blank">BootCamp Coders Student</a></em>,
+						Motorcycle Enthusiast, Comic Book & Movie & SCI FI Geek, Cat Lover
+					</p>
+			</header>
+				<main>
+					<br>
+					<h2>My Baby</h2>
+					<img src="img/myhonda.jpg" alt="Honda Shadow ACE" /> <br>
+					<h2>Penny</h2>
+					<img src="img/pennyplay.jpg" alt="Funny Cat" height="500" width="500" /> <br>
+					<h2>Lynesha</h2>
+					<img src="img/lynesha.jpg" alt="Lynesha" /> <br>
+				</main>
 		</body>
 	</html>
